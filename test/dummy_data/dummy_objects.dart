27fdab45dce1226ee98e9b87b3b0de9be2d08c83
Ibrahim Fuad <elaimbarca5@gmail.com>
2024-07-29 21:27:36 +0700

    sudah testing semua datasource

--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -1,7 +1,10 @@
 import 'package:ditonton/data/models/movie_table.dart';
+import 'package:ditonton/data/models/tv_series_table.dart';
 import 'package:ditonton/domain/entities/genre.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/movie_detail.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/entities/tv_series_detail.dart';
 
 final testMovie = Movie(
   adult: false,
@@ -20,7 +23,27 @@ final testMovie = Movie(
   voteCount: 13507,
 );
 
+final testTvSeries = Serial(
+  adult: false,
+  backdropPath: '/etj8E2o0Bud0HkONVQPjyCkIvpv.jpg',
+  genreIds: [10765, 18, 10759],
+  id: 94997,
+  originCountry: ["US"],
+  originalLanguage: "en",
+  originalName: "House of the Dragon",
+  overview:
+      "The Targaryen dynasty is at the absolute apex of its power, with more than 15 dragons under their yoke. Most empires crumble from such heights. In the case of the Targaryens, their slow fall begins when King Viserys breaks with a century of tradition by naming his daughter Rhaenyra heir to the Iron Throne. But when Viserys later fathers a son, the court is shocked when Rhaenyra retains her status as his heir, and seeds of division sow friction across the realm.",
+  popularity: 3196.313,
+  posterPath: "/t9XkeE7HzOsdQcDDDapDYh8Rrmt.jpg",
+  firstAirDate: "2022-08-21",
+  name: "House of the Dragon",
+  voteAverage: 8.4,
+  voteCount: 4530,
+);
+
 final testMovieList = [testMovie];
+
+final testTvSeriesList = [testTvSeries];
 
 final testMovieDetail = MovieDetail(
   adult: false,
@@ -37,9 +60,32 @@ final testMovieDetail = MovieDetail(
   voteCount: 1,
 );
 
+final testTvSeriesDetail = TvSeriesDetail(
+  adult: false,
+  backdropPath: 'backdropPath',
+  genres: [Genre(id: 1, name: "Sci-Fi & Fantasy")],
+  id: 1,
+  originalName: "originalName",
+  overview: "overview",
+  posterPath: "posterPath",
+  firstAirDate: "releaseDate",
+  numberOfEpisodes: 1,
+  numberOfSeasons: 1,
+  name: "name",
+  voteAverage: 1,
+  voteCount: 1,
+);
+
 final testWatchlistMovie = Movie.watchlist(
   id: 1,
   title: 'title',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testWatchlistTvSeries = Serial.watchlist(
+  id: 1,
+  name: 'name',
   posterPath: 'posterPath',
   overview: 'overview',
 );
@@ -51,9 +97,23 @@ final testMovieTable = MovieTable(
   overview: 'overview',
 );
 
+final testTvSeriesTable = TvSeriesTable(
+  id: 1,
+  name: 'name',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
 final testMovieMap = {
   'id': 1,
   'overview': 'overview',
   'posterPath': 'posterPath',
   'title': 'title',
+};
+
+final testTvSeriesMap = {
+  'id': 1,
+  'overview': 'overview',
+  'posterPath': 'posterPath',
+  'name': 'name',
 };
